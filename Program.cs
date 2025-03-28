3c3e55f52e412f94d1b1648437c4153714c8c928
ValeraVerenich <boytet68@gmail.com>
2025-03-28 17:45:04 +0300

    fixed problem

--- Program.cs
+++ Program.cs
@@ -1,117 +1,166 @@
 ﻿using System;
 using System.Net;
-using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
+using System.Diagnostics;
+using System.Threading;
 
 class Tracert
 {
-    private const int timeout = 1000; 
-    private const int maxHops = 30;    
-    private const int attempts = 3;    
+    private const int timeout = 1000;
+    private const int maxHops = 30;
+    private const int attempts = 3;
+    private const int packetSize = 32;
 
     static void Main(string[] args)
     {
         Console.Write("Введите IP-адрес или доменное имя: ");
-        string host = Console.ReadLine(); 
-
-        IPAddress ipAddress;
+        string host = Console.ReadLine();
 
         try
         {
-            
-            ipAddress = Dns.GetHostAddresses(host)[0];
-        }
-        catch (Exception)
-        {
-            Console.WriteLine($"Не удалось разрешить имя хоста или IP-адрес: {host}");
-            return;
-        }
+            IPAddress targetAddress = Dns.GetHostAddresses(host)[0];
+            Console.WriteLine($"Трассировка маршрута к {host} [{targetAddress}]");
+            Console.WriteLine($"с максимальным числом прыжков {maxHops}:");
 
-        Console.WriteLine($"Трассировка маршрута к {host} [{ipAddress}]");
-        Console.WriteLine($"с максимальным числом прыжков {maxHops}:");
-
-        using (Ping ping = new Ping())
-        {
-            PingOptions pingOptions = new PingOptions(1, true); 
-
-            for (int ttl = 1; ttl <= maxHops; ttl++)
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Icmp))
             {
-                pingOptions.Ttl = ttl;
+                socket.ReceiveTimeout = timeout;
 
-                Console.Write($"{ttl}\t");
+                byte[] receiveBuffer = new byte[256];
+                EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
 
-                long[] roundTripTimes = new long[attempts]; 
-                IPAddress replyAddress = null; 
-                string hostName = "неизвестно"; 
-
-                for (int attempt = 0; attempt < attempts; attempt++)
+                for (int ttl = 1; ttl <= maxHops; ttl++)
                 {
-                    try
-                    {
-                        
-                        PingReply reply = ping.Send(ipAddress, timeout, new byte[32], pingOptions);
+                    socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.IpTimeToLive, ttl);
+                    Console.Write($"{ttl}\t");
 
-                        if (reply.Status == IPStatus.Success || reply.Status == IPStatus.TtlExpired)
+                    IPAddress lastAddress = null;
+                    string hostName = "неизвестно";
+                    bool destinationReached = false;
+
+                    for (int attempt = 0; attempt < attempts; attempt++)
+                    {
+                        try
                         {
+                           
+                            byte[] packet = CreateIcmpEchoRequest(ttl);
+                            Stopwatch sw = Stopwatch.StartNew();
+                            socket.SendTo(packet, new IPEndPoint(targetAddress, 0));
+
+                            int bytesReceived = socket.ReceiveFrom(receiveBuffer, ref remoteEP);
+                            sw.Stop();
+
                             
-                            roundTripTimes[attempt] = reply.RoundtripTime < 1 ? 1 : reply.RoundtripTime;
-                            replyAddress = reply.Address;
-                            hostName = GetHostName(reply.Address); 
+                            if (bytesReceived >= 20)
+                            {
+                                IPAddress responseAddress = ((IPEndPoint)remoteEP).Address;
+                                lastAddress = responseAddress;
+
+                                
+                                int icmpType = receiveBuffer[20];
+                                if (icmpType == 11 || icmpType == 0)
+                                {
+                                    Console.Write($"{sw.ElapsedMilliseconds} ms\t");
+                                    destinationReached = (icmpType == 0);
+                                }
+                                else
+                                {
+                                    Console.Write("*\t");
+                                }
+                            }
+                            else
+                            {
+                                Console.Write("*\t");
+                            }
                         }
-                        else
+                        catch (SocketException ex) when (ex.SocketErrorCode == SocketError.TimedOut)
                         {
-                            roundTripTimes[attempt] = -1; 
+                            Console.Write("*\t");
+                        }
+                        catch
+                        {
+                            Console.Write("*\t");
+                        }
+
+                        Thread.Sleep(100); 
+                    }
+
+                    
+                    if (lastAddress != null)
+                    {
+                        try
+                        {
+                            hostName = Dns.GetHostEntry(lastAddress).HostName;
+                        }
+                        catch { }
+                        Console.WriteLine($"{lastAddress}\t{hostName}");
+
+                        if (destinationReached)
+                        {
+                            Console.WriteLine("Трассировка завершена.");
+                            break;
                         }
                     }
-                    catch (PingException)
+                    else
                     {
-                        roundTripTimes[attempt] = -1; 
+                        Console.WriteLine("*\tнеизвестно");
                     }
-                    catch (Exception)
-                    {
-                        roundTripTimes[attempt] = -1; 
-                    }
-                }
-
-                
-                foreach (var time in roundTripTimes)
-                {
-                    Console.Write(time == -1 ? "*\t" : $"{time} ms\t");
-                }
-
-                
-                if (replyAddress != null)
-                {
-                    Console.Write($"{replyAddress}\t{hostName}");
-                }
-                else
-                {
-                    Console.Write("*\tнеизвестно");
-                }
-
-                Console.WriteLine(); 
-
-               
-                if (replyAddress != null && replyAddress.Equals(ipAddress))
-                {
-                    Console.WriteLine("Трассировка завершена: достигнут целевой узел.");
-                    break;
                 }
             }
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
     }
 
-    
-    private static string GetHostName(IPAddress ipAddress)
+    private static byte[] CreateIcmpEchoRequest(int ttl)
     {
-        try
+        
+        byte[] packet = new byte[packetSize];
+        packet[0] = 8;  
+        packet[1] = 0;  
+        packet[2] = 0; 
+        packet[3] = 0;  
+        packet[4] = 0;  
+        packet[5] = 1;  
+        packet[6] = 0;  
+        packet[7] = 1;  
+
+        
+        for (int i = 8; i < packetSize; i++)
         {
-            IPHostEntry hostEntry = Dns.GetHostEntry(ipAddress);
-            return hostEntry.HostName;
+            packet[i] = (byte)i;
         }
-        catch (Exception)
+
+        
+        ushort checksum = CalculateChecksum(packet);
+        packet[2] = (byte)(checksum >> 8);
+        packet[3] = (byte)(checksum & 0xFF);
+
+        return packet;
+    }
+
+    private static ushort CalculateChecksum(byte[] buffer)
+    {
+        uint sum = 0;
+        int length = buffer.Length;
+        int i = 0;
+
+        while (i < length - 1)
         {
-            return "неизвестно";
+            sum += (uint)((buffer[i] << 8) + buffer[i + 1]);
+            i += 2;
         }
+
+        if (i < length)
+        {
+            sum += (uint)(buffer[i] << 8);
+        }
+
+        sum = (sum >> 16) + (sum & 0xFFFF);
+        sum += (sum >> 16);
+        return (ushort)(~sum);
     }
 }
